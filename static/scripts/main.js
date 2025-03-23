2eb4fde955d2bdf922c1d40d69f45f3640953ee5
Hamgym <pipiyakas@gmail.com>
2025-03-23 13:08:07 +0800

    not await

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,14 +5,14 @@ function frontInit() {
 		window.location.href = "/";
 	});
 
-	let list = document.querySelector(".list-item");
+	let list = document.querySelector(".list");
 	let leftBtn = document.querySelector("button.left");
 	leftBtn.addEventListener("click", function () {
-		list.scrollBy({ left: -200, behavior: "smooth" });
+		list.scrollBy({ left: -300, behavior: "smooth" });
 	});
 	let rightBtn = document.querySelector("button.right");
 	rightBtn.addEventListener("click", function () {
-		list.scrollBy({ left: 200, behavior: "smooth" });
+		list.scrollBy({ left: 300, behavior: "smooth" });
 	});
 
 	let mask = document.querySelector("div.mask");
@@ -57,23 +57,23 @@ let nextPage = null;
 let loadedPage = [];
 loadInit();
 async function loadInit() {
-	let list = document.querySelector("div.list-item");
+	let list = document.querySelector("div.list");
 	if (list.children.length == 1) {
 		let res = await fetch("./api/mrts");
 		let rawData = await res.json();
 		let mrts = rawData.data;
-		let protoDiv = list.firstElementChild;
+		let protoItem = list.firstElementChild;
 		let searchBtn = document.querySelector(".slogan button");
 		for (let mrt of mrts) {
-			let div = protoDiv.cloneNode(true);
-			div.style.display = "block";
-			div.innerText = mrt;
+			let item = protoItem.cloneNode(true);
+			item.style.display = "block";
+			item.innerText = mrt;
 			let searchField = document.querySelector(".slogan input");
-			div.addEventListener("click", function () {
-				searchField.value = div.innerText;
+			item.addEventListener("click", function () {
+				searchField.value = item.innerText;
 				searchBtn.click();
 			});
-			list.appendChild(div);
+			list.appendChild(item);
 		}
 	}
 	let attractions = document.querySelector("div.attractions");
@@ -82,7 +82,7 @@ async function loadInit() {
 	}
 	nextPage = 0;
 	loadedPage.length = 0;
-	await loadOne();
+	loadOne();
 }
 async function loadOne() {
 	if (nextPage == null || loadedPage.includes(nextPage) || loading) {
@@ -92,11 +92,11 @@ async function loadOne() {
 	loadedPage.push(nextPage);
 	let res = await fetch(`./api/attractions?page=${nextPage}&keyword=${keyword}`);
 	let rawData = await res.json();
-	let data_page = rawData.data;
-	let proto_attraction = document.querySelector("div.attraction");
+	let pageData = rawData.data;
+	let protoAttraction = document.querySelector("div.attraction");
 	let attractions = document.querySelector("div.attractions");
-	for (let data of data_page) {
-		let attraction = proto_attraction.cloneNode(true);
+	for (let data of pageData) {
+		let attraction = protoAttraction.cloneNode(true);
 		attraction.style.display = "block";
 		let name = attraction.querySelector("p.name");
 		name.innerText = data.name;
@@ -108,29 +108,28 @@ async function loadOne() {
 		mrt.innerText = data.mrt;
 		attractions.appendChild(attraction);
 	}
-	loading = false;
 	nextPage = rawData.nextPage;
+	loading = false;
 }
 
 
 let footerObserver = new IntersectionObserver(async (entries) => {
 	if (entries[0].intersectionRatio <= 0 || nextPage == null || loadedPage.includes(nextPage) || loading) {
 		return;
-	} else {
-		await loadOne();
 	}
+	loadOne();
 });
 footerObserver.observe(document.querySelector("div.footer"));
 
 
 let searchBtn = document.querySelector(".slogan button");
 let searchField = document.querySelector(".slogan input");
-searchBtn.addEventListener("click", async function () {
+searchBtn.addEventListener("click", function () {
 	if (loading) {
 		return;
 	}
 	keyword = searchField.value;
-	await loadInit();
+	loadInit();
 });
 searchField.addEventListener("keydown", function (event) {
 	if (event.key == "Enter") {
